7426750da47aa1616f3cbba8088c4147a1315e7d
Oskar Niesen <oskar-github@niesens.com>
2022-09-06 19:08:12 -0500

    Added a few more advanced tests.

---
+++ src/SnakeListAdvancedTests.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SnakeListAdvancedTests {
+    @Test
+    public void Test1() {
+        SnakeList snakeList = new SnakeList(Boolean.class, 3);
+        snakeList.add(false);
+        snakeList.add(true);
+        snakeList.add(false);
+        snakeList.add(false);
+        snakeList.add(true);
+        snakeList.add(false);
+        snakeList.remove(true);
+        snakeList.remove(false);
+        snakeList.remove(false);
+        snakeList.add(false);
+        snakeList.add(false);
+        Assert.assertArrayEquals(new Boolean[]{false, false, null, false, true, false}, snakeList.toArray());
+    }
+
+    @Test
+    public void Test2() {
+        SnakeList snakeList = new SnakeList(String.class, 0);
+        snakeList.add("alpha");
+        snakeList.add(0, "beta");
+        snakeList.add(0,"omega");
+        snakeList.add(snakeList.size,"gamma");
+        snakeList.remove(snakeList.get(1));
+        Assert.assertArrayEquals(new String[] { "omega", "alpha", "gamma", null}, snakeList.toArray());
+    }
+}
